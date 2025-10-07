5cf8451a416af0966184ea815d6330a25e9160b3
Walker <kurt.walker@capgemini.com>
2025-10-07 10:51:03 -0400

    Initial commit, working template

---
+++ VoltMotors.Client/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    basicSsl()
+  ],
+  server: {
+    port: 5173,
+    https: true,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7104',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+})
